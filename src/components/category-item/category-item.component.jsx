fad43d99dbf4f93db9a725eafcce806d740be6d7
Khoa Tran <khoa102@gmail.com>
2023-01-02 10:16:36 -0800

    Add category-item component

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,15 @@
+import "./category-item.styles.scss"
+
+const CategoryItem = ({ category }) => (
+    <div className="category-container">
+        <div className="background-image" style={{
+            backgroundImage: `url(${category.imageUrl})`
+        }}/>
+        <div className="category-body-container">
+            <h2>{category.title}</h2>
+            <p>SHOP NOW</p>
+        </div>
+    </div>
+);
+
+export default CategoryItem;
